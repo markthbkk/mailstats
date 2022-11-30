1e2a1955ab2bc3ccb52df00e2d97afb68f960f85
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-11-30 20:04:44 +0700

    new routes

--- src/v1/controllers/spamStatsController.js
+++ src/v1/controllers/spamStatsController.js
@@ -1,27 +1,23 @@
-const SpamStat = require('../models/spamStatsModel');
-
+const SpamStat = require("../models/spamStatsModel");
 
 exports.getAllSpamStats = async (req, res) => {
-  console.log('Getting Requested spamStats');
+  console.log("Getting Requested spamStats");
 
   try {
+    console.log(req.query);
 
-    console.log(req.query)
-
-    const selectedDate = req.query.date
+    const selectedDate = req.query.date;
 
     const spamStats = await SpamStat.find({ date: selectedDate });
-    
+
     const spamStatsSorted = spamStats.sort(
-      (objA, objB) =>
-        Number(objB.messageCount) -
-        Number(objA.messageCount)
+      (objA, objB) => Number(objB.messageCount) - Number(objA.messageCount)
     );
 
     res.status(200).send(spamStatsSorted);
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: "fail",
       message: err,
     });
   }
@@ -31,11 +27,8 @@ exports.getSpamStatsDistinctDates = async (req, res) => {
   console.log("Getting Requested spamStats distinct dates");
 
   try {
-    
-
     const spamStatsDates = await SpamStat.distinct("date");
 
-    
     res.status(200).send(spamStatsDates);
   } catch (err) {
     res.status(404).json({
@@ -45,3 +38,36 @@ exports.getSpamStatsDistinctDates = async (req, res) => {
   }
 };
 
+exports.getSpamStatsDistinctSenders = async (req, res) => {
+  console.log("Getting Requested spamStats distinct senders");
+
+  try {
+    const spamStatsSenders = await SpamStat.distinct("sender");
+
+    res.status(200).send(spamStatsSenders);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getSpamStatsPerUserStats = async (req, res) => {
+  console.log("Getting Requested spamStats per user stats");
+
+  const senderEmail = req.params.email;
+
+  console.log(senderEmail);
+
+  try {
+    const spamStatsSenders = await SpamStat.find({ sender: senderEmail });
+
+    res.status(200).send(spamStatsSenders);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
--- src/v1/routes/mailStatsRoutes.js
+++ src/v1/routes/mailStatsRoutes.js
@@ -16,6 +16,16 @@ router.route("/api/v1/diskStats").get(diskStatsController.getAllDiskStats);
 
 router.route("/api/v1/spamStats").get(spamStatsController.getAllSpamStats);
 
-router.route("/api/v1/spamStatsDates").get(spamStatsController.getSpamStatsDistinctDates);
+router
+  .route("/api/v1/spamStatsDates")
+  .get(spamStatsController.getSpamStatsDistinctDates);
+
+router
+  .route("/api/v1/spamStatsSenders")
+  .get(spamStatsController.getSpamStatsDistinctSenders);
+
+router
+  .route("/api/v1/spamStatsPerUserStats/user/:email")
+  .get(spamStatsController.getSpamStatsPerUserStats);
 
 module.exports = router;
